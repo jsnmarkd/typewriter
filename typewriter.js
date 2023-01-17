e93930618fdcfab8e111085fbf7002fc556c3fdc
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-17 16:03:50 -0500

    ESLint typewriter.js file

--- typewriter.js
+++ typewriter.js
@@ -2,16 +2,16 @@
 // Implement the logic to have each letter delay its appearance by 50ms (or so) longer than the previous letter.
 const sentence = "hello there from lighthouse labs";
 
-let delay = 0
+let delay = 0;
 
-for(let i=0; i<sentence.length; i++) {
+for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
     process.stdout.write(sentence[i]);
     if (i === sentence.length - 1) {
-      process.stdout.write("\n")
+      process.stdout.write("\n");
     }
-  }, delay)
-  delay += 100
+  }, delay);
+  delay += 100;
 }
 
 
